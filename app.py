b46970cd0201a3858e9ff9d62a650be6f4ddbb4d
Nana-Okrah <77549636+Nana-Okrah@users.noreply.github.com>
2021-01-29 20:57:05 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, current_app$
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return "Welcome to Nana's Rainbow project"
+
+@app.route('/red')
+def red():
+        name ="red"
+        background = "#FF0000"
+        return render_template('rainbow.html', name =$
+        
+@app.route('/orange')
+def orange():
+        name ="orange"
+        background = "orange"
+        return render_template('rainbow.html', name =$
+
+@app.route('/yellow')
+def yellow():
+        name ="yellow"
+        background = "yellow"
+        return render_template('rainbow.html', name =$
+        
+@app.route('/green')
+def green():
+        name ="green"
+        background = "green"
